321efce97698b0d1e5ec072860db15b0efa8efe2
Shane Gillespie <sgillespie@codec.ie>
2023-04-27 16:28:01 +0100

    Fixed error thrown when date is null

--- revenue-management-script.js
+++ revenue-management-script.js
@@ -6,11 +6,14 @@ var Revenue = window.Revenue || {};
     // Define some global variables
     var myUniqueId = "_myUniqueId"; // Define an ID for the notification
     var currentUserName = Xrm.Utility.getGlobalContext().userSettings.userName; // get current user name
-    var message = currentUserName + ": Your JavaScript code in action!";
+    var message = currentUserName + ": Your JavaScript code is in action!";
 
     // Code to run in the form OnLoad event
     this.formOnLoad = function (executionContext) {
         var formContext = executionContext.getFormContext();
+        //lock the end date field
+        var myField = formContext.getControl("cr30a_enddate");
+        myField.setDisabled(true);
 
         // Display the form level notification as an INFO
         formContext.ui.setFormNotification(message, "INFO", myUniqueId);
@@ -23,16 +26,20 @@ var Revenue = window.Revenue || {};
     this.attributeOnChange = function (executionContext) {
         var formContext = executionContext.getFormContext();
 
-        // Automatically set some column values if the account name contains "Contoso"
+        // Get the current start date value
         var startDate = formContext.getAttribute("cr30a_startdate").getValue();
-        // Create a new date object with the year and month from the input date
-        const endOfMonth = new Date(startDate.getFullYear(), startDate.getMonth() + 1, 0);
+        console.log("StartDate: " + startDate);
+        if(startDate !== null)
+        {
+            // Create a new date object with the year and month from the input date
+            const endOfMonth = new Date(startDate.getFullYear(), startDate.getMonth() + 1, 0);
   
-        // Set the date to the last day of the month
-        endOfMonth.setDate(endOfMonth.getDate());
+            // Set the date to the last day of the month
+            endOfMonth.setDate(endOfMonth.getDate());
   
-        // Return the end of the month date object 
-        formContext.getAttribute("cr30a_enddate").setValue(endOfMonth);
+            // Return the end of the month date object 
+            formContext.getAttribute("cr30a_enddate").setValue(endOfMonth);
+        }
     }
 
     // Code to run in the form OnSave event 
